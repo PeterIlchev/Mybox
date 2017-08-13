70ec12a396e2881e12f8d525852b6be9bae49d3e
Peter <Ilchev>
2017-08-13 19:34:05 +0300

    My Theme

---
+++ js/home.js
@@ -0,0 +1,23 @@
+
+
+/********SLICK SLIDER********/
+
+$(document).ready(function(){
+  $('.generalSlider').slick({
+  infinite: true,
+  variableWidth: true,
+  centerMode: true,
+  slidesToShow: 5,
+  slidesToScroll: 3,
+  autoplay: true,
+  arrows: true
+});
+});
+			
+
+
+
+/********FACEBOOK********/
+
+
+
